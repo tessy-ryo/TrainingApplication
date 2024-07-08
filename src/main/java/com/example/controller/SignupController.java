282fce3e435e48e6a832bfd1597860e3fdb62db9
tessymouse <tessy.ryo@gmail.com>
2024-07-08 15:34:04 +0900

    ７月８日編集

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,21 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class SignupController {
 
+	@GetMapping("/signup")
+	public String getSignup() {
+		return "user/signup";
+	}
+	
+	@PostMapping("/signup")
+	public String postSignup() {
+		return "redirect:/login";
+	}
 }
