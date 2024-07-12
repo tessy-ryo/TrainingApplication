f112c9871b074e686911ffd47a593cb06e701e16
tessymouse <tessy.ryo@gmail.com>
2024-07-12 15:46:00 +0900

    7月12日編集

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean 
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector){
+		return new MvcRequestMatcher.Builder(introspector);
+	}
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception{
+		
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers(mvc.pattern("/user/signup")).permitAll()
+                .anyRequest().authenticated()
+        );
+		
+		http.formLogin(login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .failureUrl("/login?error")
+                .usernameParameter("user")
+                .passwordParameter("pass")
+                .defaultSuccessUrl("/training/dashboard", true)
+                .permitAll()
+        ).logout(logout -> logout
+        		.logoutUrl("/logout")
+        		.logoutSuccessUrl("/login?logout")
+        		.invalidateHttpSession(true)  
+                .deleteCookies("JSESSIONID")  
+                .permitAll()
+        	
+        );
+		
+		return http.build();
+	}
+}
---
+++ src/main/java/com/example/domain/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.domain.service.impl;
+
+public class UserDetailsServiceImpl {
+
+}
