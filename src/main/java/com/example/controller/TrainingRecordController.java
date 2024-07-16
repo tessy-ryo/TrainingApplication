6ba7e99cff467402adab22930b9d6a36ae0e3c27
tessymouse <tessy.ryo@gmail.com>
2024-07-16 15:48:51 +0900

    7月16日編集

---
+++ src/main/java/com/example/controller/TrainingRecordController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.service.CustomUserDetails;
+import com.example.form.RecordWeightForm;
+
+@Controller
+@RequestMapping("/training")
+public class TrainingRecordController {
+		//**認証されたユーザーのアカウントネームを表示するメソッド*/
+		private void setupModel(Model model,Authentication authentication) {
+			CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+			model.addAttribute("username",userDetails.getAccountName());
+		}
+		
+		//**トレーニング記録画面を表示*/
+		@GetMapping("/record")
+		public String getTrainingRecord(Model model,Authentication authentication) {
+			setupModel(model,authentication);
+			//トレーニング記録画面を表示
+			return "training/record";
+		}
+		
+		//**体重を記録する画面を表示*/
+		@GetMapping("/record/weight")
+		public String getRecordWeight(Model model,@ModelAttribute RecordWeightForm form, Authentication authentication) {
+			setupModel(model,authentication);
+			//体重を記録する画面を表示
+			return "training/recordWeight";
+		}
+		
+}
---
+++ src/main/java/com/example/form/RecordWeightForm.java
@@ -0,0 +1,12 @@
+package com.example.form;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class RecordWeightForm {
+	private Date date;
+
+	private double weight;
+}
