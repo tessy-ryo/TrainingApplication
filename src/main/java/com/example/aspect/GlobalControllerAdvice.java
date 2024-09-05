20d7abcb69caae0c82f1c73975a23ee6902da4f2
tessymouse <tessy.ryo@gmail.com>
2024-09-05 15:48:57 +0900

    9月5日編集2回目

---
+++ src/main/java/com/example/aspect/GlobalControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.example.aspect;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+	@ExceptionHandler(DataAccessException.class)
+	public String dataAccessExceptionHandler(DataAccessException e,Model model) {
+		
+		model.addAttribute("error","");
+		
+		model.addAttribute("message","内部エラーが発生しました");
+		
+		model.addAttribute("status",HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return "error";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e,Model model) {
+		
+		model.addAttribute("error","");
+		
+		model.addAttribute("message","内部エラーが発生しました");
+		
+		model.addAttribute("status",HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return "error";
+	}
+}
