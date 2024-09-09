74387f317e7eb2bca33a996554002a820e7b70a0
tessymouse <tessy.ryo@gmail.com>
2024-09-09 14:56:28 +0900

    部位データをdata.sqlに追加、検索ボタンのラベルを消してplaceholderに「日付、部位または種目」を追加。

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `body_parts` (`id`,`name`,`create_date_time`,`update_date_time`) VALUES (1,'胸','2024-07-20 11:21:49','2024-07-20 11:21:49');
+INSERT INTO `body_parts` (`id`,`name`,`create_date_time`,`update_date_time`) VALUES (2,'背中','2024-07-20 11:21:49','2024-07-20 11:21:49');
+INSERT INTO `body_parts` (`id`,`name`,`create_date_time`,`update_date_time`) VALUES (3,'肩','2024-07-20 11:21:49','2024-07-20 11:21:49');
+INSERT INTO `body_parts` (`id`,`name`,`create_date_time`,`update_date_time`) VALUES (4,'脚','2024-07-20 11:21:49','2024-07-20 11:21:49');
+INSERT INTO `body_parts` (`id`,`name`,`create_date_time`,`update_date_time`) VALUES (5,'上腕二頭筋','2024-07-20 11:21:49','2024-07-20 14:24:18');
+INSERT INTO `body_parts` (`id`,`name`,`create_date_time`,`update_date_time`) VALUES (6,'上腕三頭筋','2024-07-20 11:21:49','2024-07-20 14:24:18');
+INSERT INTO `body_parts` (`id`,`name`,`create_date_time`,`update_date_time`) VALUES (7,'腹筋','2024-07-20 14:24:18','2024-07-20 14:24:18');
